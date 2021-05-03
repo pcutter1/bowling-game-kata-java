fed7de261aea1143870c970064d754a06bddc73b
Paul C <pcutter@cnm.edu>
2021-05-02 20:50:39 -0600

    Refactoring not passing perfect game test

--- src/main/java/com/galvanize/bowling/Game.java
+++ src/main/java/com/galvanize/bowling/Game.java
@@ -6,16 +6,47 @@ public class Game {
 
   private int score;
   private final ArrayList<Integer> rollScores = new ArrayList<>();
+  private int rollScoreIndex = 1;
+  private int frameRollIndex = 1;
+
+  public Game() {
+
+  }
 
   public void rollBall(int pinsHit){
     rollScores.add(pinsHit);
-    updateScore(pinsHit);
+    updateScore(frameRollIndex, pinsHit);
+    if (frameRollIndex % 2 == 1 && pinsHit == 10){
+      frameRollIndex++;
+    }
+    updateRollCount();
   }
 
-  void updateScore(int pinsHit) {
-    score += pinsHit;
+  void updateRollCount(){
+    rollScoreIndex++;
+    frameRollIndex++;
   }
 
+  void updateScore(int rollIndex, int pinsHit) {
+
+    int totalAdd = pinsHit;
+
+    if (rollIndex > 2 && rollIndex % 2 == 1
+        && rollScores.get(rollIndex - 2) + rollScores.get(rollIndex -3) == 10){
+      totalAdd+= pinsHit;
+    }
+
+    if (rollIndex > 2 && rollIndex % 2 == 1 && rollScores.get(rollIndex -3) == 10) {
+      totalAdd += rollScores.get(rollScoreIndex - 1) + pinsHit;
+    }
+
+
+    score += totalAdd;
+  }
+
+  public int getRollScoreIndex() {
+    return rollScoreIndex;
+  }
 
   public ArrayList<Integer> getRollScores() {
     return rollScores;
@@ -24,4 +55,12 @@ public class Game {
   public int getScore() {
     return score;
   }
+
+//  public boolean spareBonusActive() {
+//    return spareBonus;
+//  }
+//
+//  public boolean strikeBonusActive() {
+//    return strikeBonus;
+//  }
 }
--- src/test/java/com/galvanize/bowling/GameTest.java
+++ src/test/java/com/galvanize/bowling/GameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GameTest {
 
@@ -30,6 +31,44 @@ public class GameTest {
   }
 
   @Test
+  public void testUpdateRollCount() {
+
+    Game game = new Game();
+    game.rollBall(2);
+    game.rollBall(4);
+    game.rollBall(6);
+    game.rollBall(7);
+
+    int expected = 5;
+    int actual = game.getRollScoreIndex();
+
+
+    assertEquals(expected, actual, "After 4 rolls excluding strikes, frame count should be 5");
+  }
+
+
+
+//  @Test
+//  public void testFrameExistence() {
+//    Game game = new Game();
+//    int expected = 10;
+//    int actual = game.getFrames().length;
+//
+//    assertEquals(expected, actual, "testing the game is 10 frames long");
+//  }
+
+//  @Test
+//  public void testRollBallAddToFrameRoll1() {
+//    Game game = new Game();
+//    game.rollBall(4);
+//
+//    int expected = 4;
+//    int actual = game.getFrames()[0].roll1;
+//
+//    assertEquals(expected, actual, "testing to see if first roll goes into first frame roll1");
+//  }
+
+  @Test
   public void testAllGutters() {
     Game game = new Game();
     for (int i = 0; i < 20; i += 1) {
@@ -51,6 +90,40 @@ public class GameTest {
     assertEquals(expected, actual, "Testing 20 one-pin rolls game score will be 20");
   }
 
+//  @Test
+//  public void testSpareBonusTrigger(){
+//    Game game = new Game();
+//    game.rollBall(7);
+//    game.rollBall(3);
+//
+//    boolean actual = game.spareBonusActive();
+//
+//    assertTrue(actual);
+//  }
+
+//  @Test
+//  public void testStrikeBonusTrigger(){
+//    Game game = new Game();
+//    game.rollBall(10);
+//
+//
+//    boolean actual = game.strikeBonusActive();
+//
+//    assertTrue(actual);
+//  }
+//
+//  @Test
+//  public void testStrikeBonusTrigger2(){
+//    Game game = new Game();
+//    game.rollBall(0);
+//    game.rollBall(10);
+//
+//
+//    boolean actual = game.strikeBonusActive();
+//
+//    assertFalse(actual);
+//  }
+
   @Test
   public void testOneSpare() {
     Game game = new Game();
@@ -66,6 +139,38 @@ public class GameTest {
     int expected = 19;
     int actual = game.getScore();
     assertEquals(expected, actual, "Testing score of a game with Spare logic");
+  }
+
+  @Test
+  public void testOneStrike() {
+
+    Game game = new Game();
+    game.rollBall(10);
+    game.rollBall(3);
+    game.rollBall(3);
+
+    for (int i = 0; i < 16; i++) {
+      game.rollBall(0);
+    }
+
+    int expected = 22;
+    int actual = game.getScore();
+    assertEquals(expected, actual, "Testing score of a game with Strike logic");
+
+  }
+
+  @Test
+  public void testPerfectGame() {
+
+    Game game = new Game();
+    for (int i = 0; i < 10; i++) {
+      game.rollBall(10);
+    }
+
+    int expected = 300;
+    int actual = game.getScore();
+
+    assertEquals(expected, actual, "10 consecutive strikes should yield a score of 300");
   }
 
 
